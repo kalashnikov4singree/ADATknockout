19ecfd15aa82001c49aa7490738d3a6a9c89525a
Edgars <edgars@singree.com>
2016-06-20 17:07:46 +0300

    biomedical page

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,35 @@ $(window).ready(function(){
 $(window).resize(function(){
     fullscreenImg();
 });
+$(window).ready(function(){
+window.onload = function() {
 
+    baron({
+        root: '.main__clipper',
+        scroller: '.main__scroller',
+        bar: '.main__bar',
+        scrollingCls: '_scrolling',
+        draggingCls: '_dragging',
+        direction: 'h'
+    })
+
+    baron({
+        root: '.baron',
+        scroller: '.baron__scroller',
+        bar: '.baron__bar',
+        scrollingCls: '_scrolling',
+        draggingCls: '_dragging'
+    }).fix({
+        elements: '.header__title',
+        outside: 'header__title_state_fixed',
+        before: 'header__title_position_top',
+        after: 'header__title_position_bottom',
+        clickable: true
+    }).controls({
+
+        track: '.baron__track',
+        forward: '.baron__down',
+        backward: '.baron__up'
+    })
+}
+});
